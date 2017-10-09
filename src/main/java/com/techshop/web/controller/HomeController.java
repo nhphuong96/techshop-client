ada528f5dd17c6ae8ce2ec6af3945d880fb1b781
nhphuong96 <phuongnh.fuky@gmail.com>
2017-10-09 23:10:23 +0700

    First commit

---
+++ src/main/java/com/techshop/web/controller/HomeController.java
@@ -0,0 +1,39 @@
+package com.techshop.web.controller;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import com.google.gson.Gson;
+import com.techshop.api.entity.Category;
+import com.techshop.web.base.Constants;
+
+@ManagedBean
+@ViewScoped
+public class HomeController {
+
+	private static String GET_ALL_CATEGORY_API = Constants.PATH + "category/getAll";
+
+	public String message() {
+		return "Hello World!";
+	}
+
+	public List<Category> getAllCategory() {
+		try {
+			URL url = new URL(GET_ALL_CATEGORY_API);
+			InputStreamReader reader = new InputStreamReader(url.openStream(), "UTF-8");
+			Gson gson = new Gson();
+			Category[] categories = gson.fromJson(reader, Category[].class);
+			return Arrays.asList(categories);
+		}
+		catch(IOException e) {
+			return new ArrayList<>();
+		}
+	}
+}
